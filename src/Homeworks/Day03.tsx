2ad334ff353acb124bbeb56c84cce0d0e88e036a
YJS96 <wnt9864@hotmail.com>
2024-02-19 21:06:56 +0900

    DAY03

--- src/Homeworks/Day03.tsx
+++ src/Homeworks/Day03.tsx
@@ -8,19 +8,11 @@ export default function Day03() {
         <CustomText fontWeight={"light"} textAlign={"left"} textSize={"small"}>
           Light, Left, Small
         </CustomText>
-        <CustomText
-          fontWeight={"bold"}
-          textAlign={"center"}
-          textSize={"medium"}
-        >
+        <CustomText fontWeight={"bold"} textAlign={"center"} textSize={"medium"}>
           Bold, Center, Medium
         </CustomText>
-        <CustomText
-          fontWeight={"extrabold"}
-          textAlign={"right"}
-          textSize={"large"}
-        >
-          Extraold, Right, Large
+        <CustomText fontWeight={"extrabold"} textAlign={"right"} textSize={"large"}>
+          Extrabold, Right, Large
         </CustomText>
       </div>
     </>
